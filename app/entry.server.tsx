379e86801f14fda4b12c7eef9b2a2071c88a442a
Ryan D <git@ryan0x44.com>
2023-06-30 16:27:54 +1000

    Set up Apollo Client for GraphQL on server Per https://www.apollographql.com/blog/apollo-client/how-to-use-apollo-client-with-remix/#setting-up-the-server

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -8,6 +8,13 @@ import type { AppLoadContext, EntryContext } from "@remix-run/cloudflare";
 import { RemixServer } from "@remix-run/react";
 import isbot from "isbot";
 import { renderToReadableStream } from "react-dom/server";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { getDataFromTree } from "@apollo/client/react/ssr";
 
 export default async function handleRequest(
   request: Request,
@@ -16,8 +23,39 @@ export default async function handleRequest(
   remixContext: EntryContext,
   loadContext: AppLoadContext
 ) {
+  const client = new ApolloClient({
+    ssrMode: true,
+    cache: new InMemoryCache(),
+    link: createHttpLink({
+      uri: "https://flyby-gateway.herokuapp.com/", // from Apollo's Voyage tutorial series (https://www.apollographql.com/tutorials/voyage-part1/)
+      //headers: request.headers,
+      credentials: request.credentials ?? "include", // or "same-origin" if your backend server is the same domain
+    }),
+  });
+
+  const App = (
+    <ApolloProvider client={client}>
+      <RemixServer context={remixContext} url={request.url} />
+    </ApolloProvider>
+  )
+
+  await getDataFromTree(App)
+  // Extract the entirety of the Apollo Client cache's current state
+    const initialState = client.extract();
+
   const body = await renderToReadableStream(
-    <RemixServer context={remixContext} url={request.url} />,
+    (
+      <>
+        {App}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.__APOLLO_STATE__=${JSON.stringify(
+              initialState
+            ).replace(/</g, "\\u003c")}`, // The replace call escapes the < character to prevent cross-site scripting attacks that are possible via the presence of </script> in a string literal
+          }}
+        />
+      </>
+    ),
     {
       signal: request.signal,
       onError(error: unknown) {
@@ -37,4 +75,5 @@ export default async function handleRequest(
     headers: responseHeaders,
     status: responseStatusCode,
   });
+
 }
