58db970b9211fc37208b468318cddad5e55a6627
Ryan D <git@ryan0x44.com>
2023-06-30 16:39:58 +1000

    Made TypeScript happy Per https://www.apollographql.com/blog/apollo-client/how-to-use-apollo-client-with-remix/#make-typescript-happy

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,3 +1,6 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/cloudflare" />
 /// <reference types="@cloudflare/workers-types" />
+interface Window {
+    __APOLLO_STATE__: any
+}
