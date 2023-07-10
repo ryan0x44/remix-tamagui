3437045209e3ad9efd319420d6856bba6604c979
Ryan D <git@ryan0x44.com>
2023-07-10 16:11:39 +1000

    Added example of using graphql-request

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,8 @@
 import type { V2_MetaFunction } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+import { graphql } from '../graphql/gql';
+import { gql, GraphQLClient, request } from 'graphql-request'
+import { useQuery } from '@tanstack/react-query';
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -7,10 +11,34 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
+const locationsQuery = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+    }
+  }
+`;
+
+export async function loader() {
+  const client = new GraphQLClient(
+    'https://flyby-locations-sub.herokuapp.com/',
+    // @see https://github.com/remix-run/remix/issues/2075#issuecomment-1048934060
+    { fetch: fetch }
+  );
+  return await client.request(locationsQuery)
+}
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
+      <div>
+        { JSON.stringify(data.locations) }
+      </div>
       <ul>
         <li>
           <a
